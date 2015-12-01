0e8d410acde2d33b877e7690e860a37421ed58a9
Platon <platonov256@gmail.com>
2015-12-01 17:00:09 +0800

    readme

--- src/projectsamsungserver/Location.java
+++ src/projectsamsungserver/Location.java
@@ -10,55 +10,57 @@ import java.util.ArrayList;
 
 public class Location
 {
-	private ArrayList<Item> items;
-	private ArrayList<ICreature> members;
-	private int x,y;
-	
-	public ArrayList<Item> getItems()
-	{
-		return this.items;
-	}
-	
-	public void addItem(Item item)
-	{
-		this.items.add(item);
-	}
-	
-	public void removeItem(Item item)
-	{
-		this.items.remove(item);
-	}
-	
-	public void addmember(ICreature member)
-	{
-		this.members.add(member);
-	}
-	
-	public void removeMember(ICreature member)
-	{
-		this.members.remove(member);
-	}
-	
+
+    private ArrayList<Item> items;
+    private ArrayList<ICreature> members;
+    private int x, y;
+
+    public ArrayList<Item> getItems()
+    {
+        return this.items;
+    }
+
+    public void addItem(Item item)
+    {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item)
+    {
+        this.items.remove(item);
+    }
+
+    public void addmember(ICreature member)
+    {
+        this.members.add(member);
+    }
+
+    public void removeMember(ICreature member)
+    {
+        this.members.remove(member);
+    }
+
     public Location(int x, int y)
     {
-    	this.setX(x);
-    	this.setY(y);
+        this.x = x;
+        this.y = y;
+        this.items = new ArrayList<>();
+        this.members = new ArrayList<>();
     }
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
 
-	public ArrayList<ICreature> getMembers() {
-		return this.members;
-	}
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public ArrayList<ICreature> getMembers()
+    {
+        return this.members;
+    }
 
 }
